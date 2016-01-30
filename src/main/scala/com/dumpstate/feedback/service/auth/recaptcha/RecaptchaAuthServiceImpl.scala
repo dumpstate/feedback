9a3ef57322dfc363a56adcd5c46547e77b881a23
dumpstate <dumpstate@outlook.com>
2016-01-30 18:58:09 +0100

    Fix call to reCaptcha

--- src/main/scala/com/dumpstate/feedback/service/auth/recaptcha/RecaptchaAuthServiceImpl.scala
+++ src/main/scala/com/dumpstate/feedback/service/auth/recaptcha/RecaptchaAuthServiceImpl.scala
@@ -6,29 +6,29 @@ import scala.concurrent.Future.successful
 
 import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
-import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
 import akka.http.scaladsl.model._
+import akka.http.scaladsl.model.Uri.Query
 import akka.http.scaladsl.unmarshalling.Unmarshal
 import akka.stream.Materializer
 import org.slf4j.Logger
-import spray.json._
 
 import com.dumpstate.feedback.dto.Secret
 import com.dumpstate.feedback.service.auth.AuthService
 
 class RecaptchaAuthServiceImpl(logger: Logger)(
-  implicit system: ActorSystem, fm: Materializer) extends AuthService with SprayJsonSupport {
+  implicit system: ActorSystem, fm: Materializer) extends AuthService {
 
-  val http = Http(system)
+  private val http = Http(system)
+
+  private def recaptchaUri(token: String, secret: Secret) =
+    Uri("https://www.google.com/recaptcha/api/siteverify").withQuery(
+      Query(("secret" -> secret.value), ("response" -> token)))
 
   override def authenticate(token: String, secret: Secret): Future[Boolean] =
     http.singleRequest(
       HttpRequest(
         method = HttpMethods.POST,
-        uri = "https://www.google.com/recaptcha/api/siteverify")
-        .withEntity(
-          ContentTypes.`application/json`,
-          RecaptchaEnvelope(secret, token).toJson.compactPrint))
+        uri = recaptchaUri(token, secret)))
       .flatMap {
         case HttpResponse(StatusCodes.OK, _, res, _) =>
           Unmarshal(res).to[RecaptchaResponse]
